5a840b170e2b3a6a0c6e96345aaac3b6e10fb056
nrlquaker <ustynov.vy@gmail.com>
2017-09-18 17:22:35 +0300

    Update colorpicker-launcher to v0.3

--- Casks/colorpicker-launcher.rb
+++ Casks/colorpicker-launcher.rb
@@ -1,12 +1,14 @@
 cask 'colorpicker-launcher' do
-  version '0.1'
-  sha256 'c9d36506c45c86ddcccbc94ca852b9a531e7b0194949216e26bf463bc988a477'
+  version '0.3'
+  sha256 'abbbcbdbd69dca10858f322796fb08c39dbbeaabd17fac9fa7c4036ad69106da'
 
-  url "https://github.com/nrlquaker/color-picker-launcher/archive/v#{version}.zip"
+  url "https://github.com/nrlquaker/color-picker-launcher/releases/download/v#{version}/color.picker.v#{version}.zip"
   appcast 'https://github.com/nrlquaker/color-picker-launcher/releases.atom',
-          checkpoint: '80c2518b5c7fc077f05bff19dd3b2b3020325931a8ac63018c9dd69f6b228147'
+          checkpoint: '05f0935ff99feaeb38f1278514799c9ee602279253963ed5358768b54d1752af'
   name 'Color Picker'
   homepage 'https://github.com/nrlquaker/color-picker-launcher'
 
-  app "color-picker-launcher-#{version}/Color Picker.app"
+  app 'Color Picker.app'
+
+  zap trash: '~/Library/Preferences/com.apple.ScriptEditor.id.Color-picker.plist'
 end
