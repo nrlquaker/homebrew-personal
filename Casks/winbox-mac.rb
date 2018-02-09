f2a0485430f072410fcf5923f4e84edbe9d1d9db
nrlquaker <ustynov.vy@gmail.com>
2018-02-09 17:37:11 +0200

    Update winbox-mac to 3.12

--- Casks/winbox-mac.rb
+++ Casks/winbox-mac.rb
@@ -1,10 +1,10 @@
 cask 'winbox-mac' do
-  version '3.11'
-  sha256 '9bf39b541d1d84d0dbf05863eb814062faa16e3a0fb03ca12cc720fef903d7ad'
+  version '3.12'
+  sha256 '1ed736b9021288d119799b9d22fb0a2b2c66a3e0525c8a83631963c18b934365'
 
   url "https://github.com/nrlquaker/winbox-mac/releases/download/v#{version}/Winbox-mac-#{version}.zip"
   appcast 'https://github.com/nrlquaker/winbox-mac/releases.atom',
-          checkpoint: '543cc8e6905c96b123cc082a660bdf58a990296ed83e1f33786bcaaa7ebc007c'
+          checkpoint: '7e246a1897b912fa834e185fa96fa6db5195c8da805b390ce4f16d8f8dedd393'
   name 'Winbox-mac'
   homepage 'https://github.com/nrlquaker/winbox-mac/'
 
