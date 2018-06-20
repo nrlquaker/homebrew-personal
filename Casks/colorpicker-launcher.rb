7ee252496ed5ef3fe6a329c30af00d8ee324c8c6
nrlquaker <ustynov.vy@gmail.com>
2018-06-21 01:01:14 +0300

    Remove appcast checkpoints

--- Casks/colorpicker-launcher.rb
+++ Casks/colorpicker-launcher.rb
@@ -3,8 +3,7 @@ cask 'colorpicker-launcher' do
   sha256 'abbbcbdbd69dca10858f322796fb08c39dbbeaabd17fac9fa7c4036ad69106da'
 
   url "https://github.com/nrlquaker/color-picker-launcher/releases/download/v#{version}/color.picker.v#{version}.zip"
-  appcast 'https://github.com/nrlquaker/color-picker-launcher/releases.atom',
-          checkpoint: '05f0935ff99feaeb38f1278514799c9ee602279253963ed5358768b54d1752af'
+  appcast 'https://github.com/nrlquaker/color-picker-launcher/releases.atom'
   name 'Color Picker'
   homepage 'https://github.com/nrlquaker/color-picker-launcher'
 
