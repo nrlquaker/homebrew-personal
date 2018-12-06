d721a07a2277984658f286360a792dfce8538222
nrlquaker <ustynov.vy@gmail.com>
2018-12-06 17:47:14 +0200

    Update winebottler to 2.0 devel

--- Casks/winebottler.rb
+++ Casks/winebottler.rb
@@ -1,11 +1,11 @@
 cask 'winebottler' do
-  version '1.8.2'
-  sha256 'a82de7404802e1d422b7ee9fb9b3a5444fe922736ea5f8eb182141fad46b3fea'
+  version '2.0'
+  sha256 '725955d89b36e8ef36abf4a56db45e15de16a827ddecc7edc88e88ff440c3390'
 
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/nrlquaker/homebrew-personal-binaries/raw/master/Bin/WineBottlerCombo_#{version}.dmg"
   name 'WineBottler'
-  homepage 'http://winebottler.kronenberg.org/'
+  homepage 'https://winebottler.kronenberg.org/'
 
   app 'WineBottler.app'
   app 'Wine.app'
